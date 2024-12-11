1c1e6fe7731f2dc389540acd3505749c6f5b6833
Sourav Bandyopadhyay <souravb.1998@gmail.com>
2024-12-11 09:26:10 +0530

    Added public folder and using Dockerfile to build image

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-   /**
-     * Using the tailingSlash option is recommended when exporting it to S3
-     * since S3 is only able to rewrite routes to /index.html
-     */
-   trailingSlash: true,
+  /**
+   * Using the tailingSlash option is recommended when exporting it to S3
+   * since S3 is only able to rewrite routes to /index.html
+   */
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
